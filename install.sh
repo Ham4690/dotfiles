1058b3049450f9b74a23ba92b6e89bf64396d7aa
Arata Higashiguchi <ham46903@gmail.com>
2025-04-29 09:20:11 +0900

    init install.sh

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# dotfilesのパスを取得
+DOTFILES_DIR="$HOME/dotfiles"
+
+# シンボリックリンク作成関数
+link_file() {
+  local src=$1
+  local dest=$2
+
+  if [ -e "$dest" ] || [ -L "$dest" ]; then
+    echo "⚠️  $dest already exists. Skipping."
+  else
+    ln -s "$src" "$dest"
+    echo "✅ Linked $dest -> $src"
+  fi
+}
+
+# .zshrcのリンク
+# link_file "$DOTFILES_DIR/zshrc" "$HOME/.zshrc"
+
+# .vimrcのリンク
+link_file "$DOTFILES_DIR/vimrc" "$HOME/.vimrc"
+
